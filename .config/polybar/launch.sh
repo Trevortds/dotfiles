e1aee19eff1cf8a18933fbd6ed7806229671ac28
Trevor Sullivan <Trevor.Sullivan@ayfie.com>
2018-04-16 13:19:10 -0400

    finished polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-MONITOR="HDMI-0" polybar bigbar &
-MONITOR="DVI-D-0" polybar traybar &
+MONITOR="DP-1-1-2" polybar bigbar &
+MONITOR="eDP-1-1" polybar traybar &
+MONITOR="DP-1-2-2" polybar minibar &
 
 echo "Bars launched..."
