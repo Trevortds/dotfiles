e6c21965a6fdfeaff560007699a0c211c2e60759
trevortds <trevortds3@gmail.com>
2018-04-21 08:46:48 -0400

    added littlebar and removed borders

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,5 +9,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 MONITOR="HDMI-0" polybar bigbar &
 MONITOR="DVI-D-0" polybar traybar &
+MONITOR="DP-1" polybar littlebar &
 
 echo "Bars launched..."
