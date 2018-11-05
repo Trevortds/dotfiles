6b7781d17994ffc944cb6e12ac1783c9c181e605
Trevor Sullivan <Trevor.Sullivan@ayfie.com>
2018-11-05 14:06:32 -0500

    added secrets file for slack stuff

--- .config/i3/mute.sh
+++ .config/i3/mute.sh
@@ -7,3 +7,7 @@ sleep 10
 # suspend message display
 pkill -u "trevor" -USR1 dunst
 
+source /home/trevor/.config/i3/secrets
+
+# put slack thing
+http https://slack.com/api/users.profile.set token==$SLACK_TOKEN profile=='{"status_text":"checking slack only on the hour, call if its urgent","status_emoji":":mute:"}'
--- .config/i3/unmute.sh
+++ .config/i3/unmute.sh
@@ -4,3 +4,9 @@
 pkill -u "trevor" -USR2 dunst
 
 notify-send "Time to answer slack messages!"
+
+source /home/trevor/.config/i3/secrets
+
+# put slack thing
+http https://slack.com/api/users.profile.set token==$SLACK_TOKEN profile=='{"status_text":"available until 10 past","status_emoji":":loud_sound:"}'
+
